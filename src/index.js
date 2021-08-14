e4780713e9eec18ae64b3cfea6fe2170eb53fcd2
Ihar Kozik <ihar.kozik@gmail.com>
2021-08-14 16:59:21 +0300

    feat: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array === 'object') {
+        if (array.length == 0) {
+            return 0;
+        } else {
+            return Math.min.apply(null, array);
+        }
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array === 'object') {
+        if (array.length == 0) {
+            return 0;
+        } else {
+            return Math.max.apply(null, array);
+        }
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array === 'object') {
+        if (array.length == 0) {
+            return 0;
+        } else {
+            return Object.values(array).reduce((a, b) => a + b, 0) / array.length;
+        }
+    } else {
+        return 0;
+    }
 }
